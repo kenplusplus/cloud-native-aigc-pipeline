86a769eab388d18e9ae76b5086471a3495ce845f
Lu, Ken <ken.lu@intel.com>
2023-07-27 14:42:05 +0800

    models: update model download description
    Signed-off-by: Lu, Ken <ken.lu@intel.com>

---
+++ models/README.md
@@ -0,0 +1,17 @@
+# LLM models
+
+## Download Models
+
+THe default models are saved at <http://css-devops.sh.intel.com/download/aigc/models/>.
+
+- To download a model:
+
+```
+./get_models.sh opt-1.3b-bf16-8b-samples
+```
+
+- To download all models:
+
+```
+./get_models.sh all
+```
--- models/get_models.sh
+++ models/get_models.sh
@@ -6,7 +6,10 @@ MODEL_DOWNLOAD_URL="http://css-devops.sh.intel.com/download/aigc/models/"
 
 MODELS=("fastchat-t5-3b-v1.0" "vicuna-7b-v1.3" )
 
-for model_name in ${MODELS[@]}; do
+
+download_a_model() {
+    model_name=$1
+
     if [[ ! -f ${CURR_DIR}/${model_name}.tar.gz ]]; then
         wget -O ${CURR_DIR}/${model_name}.tar.gz ${MODEL_DOWNLOAD_URL}/${model_name}.tar.gz
     fi
@@ -15,6 +18,15 @@ for model_name in ${MODELS[@]}; do
         ${CURR_DIR}/../tools/tarx -d ${CURR_DIR}/${model_name}.tar.gz
         echo "Successful decompress the ${model_name}.tar.gz ==> ${CURR_DIR}/${model_name}..."
     fi
+}
+
+if [[ ${1} == "all" ]]; then
+    for model_name in ${MODELS[@]}; do
+        download_a_model $model_name
+        echo "Model ${model_name} is ready at ${CURR_DIR}/${model_name}"
+    done
+else
+    download_a_model $1
 
     echo "Model ${model_name} is ready at ${CURR_DIR}/${model_name}"
-done
+fi
