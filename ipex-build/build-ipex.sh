28943ff24c12b092e271da22d9b5f00ef5b849d9
Lu Ken <ken.lu@intel.com>
2023-08-02 10:04:48 +0800

    refine
    Signed-off-by: Lu Ken <ken.lu@intel.com>

---
+++ ipex-build/build-ipex.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+CURR_DIR=$(readlink -f "$(dirname "$0")")
+
+VER_IPEX="v2.1.0.dev+cpu.llm"
+
+LLVM_ROOT=/llvm/
+
+MAX_JOBS_VAR=$(nproc)
+if [ ! -z "${MAX_JOBS}" ]; then
+    MAX_JOBS_VAR=${MAX_JOBS}
+fi
+
+CONDA_PREFIX=/opt/conda/
+# Compile individual component
+export CC=${CONDA_PREFIX}/bin/gcc
+export CXX=${CONDA_PREFIX}/bin/g++
+export LD_PRELOAD=${CONDA_PREFIX}/lib/libstdc++.so
+
+# Checkout individual components
+if [ ! -d intel-extension-for-pytorch ]; then
+    wget http://css-devops.sh.intel.com/download/mirror/intel-extension-for-pytorch/intel-extension-for-pytorch-2023_07_31.tar.gz
+    tar zxvf intel-extension-for-pytorch-2023_07_31.tar.gz
+    git config --global --add safe.directory /ipex/intel-extension-for-pytorch
+fi
+
+cd intel-extension-for-pytorch
+git checkout ${VER_IPEX}
+git submodule sync
+git submodule update --init --recursive
+cd ..
+
+ln -s ${LLVM_ROOT}/bin/llvm-config ${LLVM_ROOT}/bin/llvm-config-13
+
+export USE_LLVM=${LLVM_ROOT}
+export LLVM_DIR=${USE_LLVM}/lib/cmake/llvm
+export DNNL_GRAPH_BUILD_COMPILER_BACKEND=1
+export IPEX_VERSION=2.1.0.dev0+cpu.llm
+export IPEX_VERSIONED_BUILD=0
+CXXFLAGS_BK=${CXXFLAGS}
+export CXXFLAGS="${CXXFLAGS} -D__STDC_FORMAT_MACROS"
+
+cd intel-extension-for-pytorch
+python setup.py clean
+python setup.py bdist_wheel
+export CXXFLAGS=${CXXFLAGS_BK}
+unset IPEX_VERSIONED_BUILD
+unset IPEX_VERSION
+unset DNNL_GRAPH_BUILD_COMPILER_BACKEND
+unset LLVM_DIR
+unset USE_LLVM
+python -m pip install --force-reinstall dist/*.whl
